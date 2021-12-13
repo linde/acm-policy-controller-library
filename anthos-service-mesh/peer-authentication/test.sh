49e921d9e377f463f82908ead9e6659e812feff6
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-13 18:35:39 +0800

    Delete anthos-service-mesh directory

--- anthos-service-mesh/peer-authentication/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run -v $(pwd):/source gcr.io/config-management-release/read-yaml -i /dev/null -d source_dir=/source | docker run -i gcr.io/config-management-release/policy-controller-validate
